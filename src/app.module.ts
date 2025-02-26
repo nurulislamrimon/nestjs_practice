6d413fcf002d9c37c1579ada0a5c14d9efbb41d4
nurulislamrimon <nurulislamrimon@gmail.com>
2025-02-26 13:12:12 +0600

    db setup with env expandVariables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './config/db';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+    TypeOrmModule.forRoot(dbConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/env.ts
@@ -0,0 +1,8 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const env = {
+  port: process.env.PORT || 5000,
+  db_pass: process.env.DB_PASS,
+};
